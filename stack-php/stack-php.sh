e032e8edf4d547240fe35d83e7b885bd5080468a
mlamamra <mlamamra@dawan.fr>
2025-04-01 10:11:05 +0200

    J2: communication nginx + php avec volumes

--- stack-php/stack-php.sh
+++ stack-php/stack-php.sh
@@ -11,9 +11,20 @@
 
 
 #### CONTENEURS #######
+# communication entre les conteneurs
+# 1/ configurer le vhost (serverName et fastcgi_pass)
+# 2/ injecter le vhost dans le conteneur nginx
+# 3/ injecter le fichier index.php dans le conteneur php-fpm
+
+docker run \
+       --name stack-php-fpm \
+       -d --restart unless-stopped \
+       -v ./index.php:/srv/index.php \
+       bitnami/php-fpm:8.4-debian-12
 
 docker run \
        --name stack-php-nginx \
        -d --restart unless-stopped \
        -p 8081:80 \
+       -v ./vhost.conf:/etc/nginx/conf.d/vhost.conf \
        nginx:1.27.4-perl
